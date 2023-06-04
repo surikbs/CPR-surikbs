f16d51c2fb6ab36ee7b4b1513723732d1db74aeb
surikbs <surikbs@gmail.com>
2023-06-05 01:59:53 +0530

    changes

--- src/core/Controller.py
+++ src/core/Controller.py
@@ -2,33 +2,33 @@ import logging
 
 from config.Config import getBrokerAppConfig
 from models.BrokerAppDetails import BrokerAppDetails
-from loginmgmt.AlpacaLogin import AlpacaLogin
+from loginmgmt.ZerodhaLogin import ZerodhaLogin
 
 class Controller:
-    brokerLogin = None # static variable
-    brokerName = None # static variable
+  brokerLogin = None # static variable
+  brokerName = None # static variable
 
-    def handleBrokerLogin(args):
-        brokerAppConfig = getBrokerAppConfig()
+  def handleBrokerLogin(args):
+    brokerAppConfig = getBrokerAppConfig()
 
-        brokerAppDetails = BrokerAppDetails(brokerAppConfig['broker'])
-        brokerAppDetails.setClientID(brokerAppConfig['clientID'])
-        brokerAppDetails.setAppKey(brokerAppConfig['appKey'])
-        brokerAppDetails.setAppSecret(brokerAppConfig['appSecret'])
+    brokerAppDetails = BrokerAppDetails(brokerAppConfig['broker'])
+    brokerAppDetails.setClientID(brokerAppConfig['clientID'])
+    brokerAppDetails.setAppKey(brokerAppConfig['appKey'])
+    brokerAppDetails.setAppSecret(brokerAppConfig['appSecret'])
 
-        logging.info('handleBrokerLogin appKey %s', brokerAppDetails.appKey)
-        Controller.brokerName = brokerAppDetails.broker
-        if Controller.brokerName == 'alpaca':
-            Controller.brokerLogin = AlpacaLogin(brokerAppDetails)
-        # Other brokers - not implemented
-        #elif Controller.brokerName == 'fyers':
-        #Controller.brokerLogin = FyersLogin(brokerAppDetails)
+    logging.info('handleBrokerLogin appKey %s', brokerAppDetails.appKey)
+    Controller.brokerName = brokerAppDetails.broker
+    if Controller.brokerName == 'zerodha':
+      Controller.brokerLogin = ZerodhaLogin(brokerAppDetails)
+    # Other brokers - not implemented
+    #elif Controller.brokerName == 'fyers':
+      #Controller.brokerLogin = FyersLogin(brokerAppDetails)
 
-        redirectUrl = Controller.brokerLogin.login(args)
-        return redirectUrl
+    redirectUrl = Controller.brokerLogin.login(args)
+    return redirectUrl
 
-    def getBrokerLogin():
-        return Controller.brokerLogin
+  def getBrokerLogin():
+    return Controller.brokerLogin
 
-    def getBrokerName():
-        return Controller.brokerName
+  def getBrokerName():
+    return Controller.brokerName
